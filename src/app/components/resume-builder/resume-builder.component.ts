0d33ad348c8aa52a984adf02e53699e946b8128d
niloymcs17 <niloy.mcs17.du@gmail.com>
2022-07-17 10:07:43 +0530

    not required

--- src/app/components/resume-builder/resume-builder.component.ts
+++ src/app/components/resume-builder/resume-builder.component.ts
@@ -10,25 +10,7 @@ export class ResumeBuilderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.rankTeams(["ABC","ACB","ABC","ACB","ACB"]);
   }
-
-
- rankTeams(votes:any) {
-  let siz= votes[0].length;
-  // let matrix = new Array(siz).fill(0).map(() => new Array(26).fill(0));
-      let matrix = new Array(26).fill(0);
-  let i=0,j=0;
-  
-  for(i=0;i<votes.length;i++){
-      let str = votes[i];
-      for(j=0;j<siz;j++){
-          let c =str.charAt(j);
-          matrix[c.charCodeAt(0)-('A').charCodeAt(0)]+= 100*(j+1);
-      }
-  }
-    
-};
 
 
 }
