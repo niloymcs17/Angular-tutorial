abdef42247b36820bad5a45c14cc92ab628ea10b
niloymcs17 <niloy.mcs17.du@gmail.com>
2022-08-29 23:36:17 +0530

    added different sections

--- src/app/modules/resume/resume.module.ts
+++ src/app/modules/resume/resume.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { ResumeBuilderComponent } from "./resume-builder/resume-builder.component";
 import { ResumeRouting } from "./resume.module-routing";
+import {MatDividerModule} from '@angular/material/divider';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
     declarations: [
-        ResumeBuilderComponent
+        ResumeBuilderComponent,
+        ExperienceComponent,
+        EducationComponent,
+        ProjectsComponent
     ],
     imports: [
       MatIconModule,
-      ResumeRouting
+      ResumeRouting,
+      MatDividerModule
     ]
   })
   export class ResumeModule { }
